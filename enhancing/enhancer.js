14557c2a37886444cd2d4b48b8b744aaa197020d
Parimala Vemula <parimala.thodupunoori@gmail.com>
2020-08-20 00:07:48 -0700

    Testing JS

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,18 +5,74 @@ module.exports = {
   get,
 };
 
+// ### Items.
+
+// - Items have `name`, `durability` and `enhancement`.
+// - The item's `enhancement` it's a number from 0 to 20.
+// - The item's `durability` it's a number from 0 to 100.
+
+// ### When enhancement succeeds
+
+// - The item's enhancement increases by 1.
+// - If the item enhancement level is 20, the enhancement level is not changed.
+// - The durability of the item is not changed.
+ 
 function success(item) {
-  return { ...item };
-}
+  if(item.enhancement === 20) {
+    return {...item};
+  } else {
+    return {...item, enhancement: item.enhancement + 1}
+  }
+};
+// function success(item) {
+//   let successItem = {
+//     name: item.name,
+//     enhancement: item.enhancement,
+//     durability: item.durability
+//   }
+//   if( successItem.enhancement === 20) {
+//     return successItem;
+//   } else {
+//     successItem.enhancement++;
+//   }
+//   return {...successItem};
+// };
+
+// ### When enhancement fails
+
+// - If the item's enhancement is less than 15, the durability of the item is decreased by 5.
+// - If the item's enhancement is 15 or more, the durability of the item is decreased by 10.
+// - If the item's enhancement level is greater than 16, the enhancement level decreases by 1 (17 goes down to 16, 18 goes down to 17).
 
 function fail(item) {
-  return { ...item };
-}
+  let checkItem = { ...item}
+  if (checkItem.enhancement > 16 ) {
+    checkItem.enhancement --;
+    checkItem.durability  = checkItem.durability - 10;
+  } else if (checkItem.enhancement >= 15) {
+    checkItem.durability  = checkItem.durability - 10;
+  } else {
+    checkItem.durability = checkItem.durability - 5;
+  }
+  return checkItem;
+};
+
+// a `repair(item)` method that accepts an `item` object and **returns a new item** with the durability restored to 100. 
+// This method is the simplest of the three and would be a **good starting point** on this project.
 
 function repair(item) {
-  return { ...item };
-}
+  // const repairedItem = {
+  //   name: item.name,
+  //   enhancement: item.enhancement,
+  //   durability: 100,
+  // }
+  // return repairedItem;
+  return { ...item, durability: 100};
+};
 
+// if the enhancement level is 0, the the name is not modified.
+// if the enhancement level is greater than 0. 
+// Example: the name of a "Iron Sword" enhanced to 7 would be "[+7] Iron Sword".
 function get(item) {
   return { ...item };
-}
+};
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,94 @@
-const enhancer = require('./enhancer.js');
+const { success,
+    fail,
+    repair,
+    get,} = require('./enhancer.js');
 // test away!
+
+describe("item's durability and enhancement", () =>{
+    describe("repair", () => {
+        it('to check if an item is repaired or durable', () => {
+            expect(repair({
+                name: "game",
+                enhancement: 15,
+                durability: 70
+            })).toEqual({
+                name: "game",
+                enhancement: 15,
+                durability: 100
+            });
+        });
+        let o = {
+            name: "game",
+            enhancement: 15,
+            durability: 70
+        };
+        it('to check if new object is being returned', () => {
+            expect(repair(o)).not.toBe(o)
+        });
+        it('to check if original object is not modified', () => {
+            repair(o);
+            expect(o).toEqual({
+                name: "game",
+                enhancement: 15,
+                durability: 70
+            })
+        })
+    });
+    describe("success", () => {
+        let o = {
+            name: "game",
+            enhancement: 15,
+            durability: 70
+        };
+        it('to check if new object is being returned', () => {
+            expect(success(o)).not.toBe(o)
+        });
+        it('to check if original object is not modified', () => {
+            success(o);
+            expect(o).toEqual({
+                name: "game",
+                enhancement: 15,
+                durability: 70
+            })
+        })
+        it('to check if an item is enhanced or successful', () => {
+            expect(success(o)).toEqual({...o, enhancement: o.enhancement + 1});
+            // expect({enhancement: 20}).toEqual({enhancement: 20});
+        });
+        it('to check if max enhancement is 20', () => {
+            expect(success({...o, enhancement: 20})).toEqual({...o, enhancement: 20});
+        });
+    });
+    describe("fail", () => {
+        let o = {
+            name: "game",
+            enhancement: 15,
+            durability: 70
+        };
+        it('to check if new object is being returned', () => {
+            expect(fail(o)).not.toBe(o)
+        });
+        it('to check if original object is not modified', () => {
+            fail(o);
+            expect(o).toEqual({
+                name: "game",
+                enhancement: 15,
+                durability: 70
+            })
+        })
+        it('to check if enhancement is decreased', () => {
+            expect(fail({...o, enhancement: 18})).toEqual({...o, enhancement: 17, durability: 60});
+        })
+        it('to check if an item durability is decreased', () => {
+            expect(fail({...o, enhancement: 16})).toEqual({...o, enhancement: 16, 
+                durability: o.durability - 10});
+        })
+        it('to check if an item is failed or enhancement is decreased', () => {
+            expect(fail(o)).toEqual({...o, durability: o.durability - 10});
+        })
+        it("to check if an item's durability is decreased", () => {
+            expect(fail({...o, enhancement: 14})).toEqual({...o, enhancement: 14,
+                    durability: o.durability - 5});
+        })
+    });
+});
